321076643728542a178394c585b0191da28439ba
gzk <gzk@123.com>
2022-05-31 20:09:02 +0800

    first commit

---
+++ src/TestGit.java
@@ -0,0 +1,9 @@
+/**
+ * @author shakstart
+ * @create 2022-05-31-20:07
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
